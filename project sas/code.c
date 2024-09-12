ec49b2f68dc5c6151f5140b68c9b152566afba2c
yassine <uzii8712@gmail.com>
2024-09-12 15:40:48 +0200

    start the trie

--- project sas/code.c
+++ project sas/code.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 struct etudiants{
     int id;
     char nom[100];
@@ -25,6 +27,8 @@ void menu(){
     printf("\t 4- afficher  les informations completes des etudiants, : \n");
     printf("\t 5- Calculer la moyenne generale : \n");
     printf("\t 6- afficher les statistiques : \n");
+    printf("\t 7- Rechercher un ewtudiant par son nom : \n");
+    printf("\t 8- Afficher la liste des étudiants inscrits dans un département spécifique: \n");
 }
 
 void ajouter(){
@@ -59,6 +63,8 @@ for(int j = 0 ; j < numEtediant ; j++){
     int res = strcmp(etudiant[j].nom, name);
 
     if(res == 0){
+            printf("----------------------------------------- \n");
+
             printf("le nom : %s : \n", etudiant[j].nom);
 
             printf("entrer votre prenom : %s \n",etudiant[j].prenom);
@@ -67,7 +73,7 @@ for(int j = 0 ; j < numEtediant ; j++){
 
             printf("entrer la departement : %s \n",etudiant[j].departement);
 
-            printf("entrer la note generale : %f \n", etudiant[j].noteGenerale);
+            printf("entrer la note generale : %.2f \n", etudiant[j].noteGenerale);
 
             printf("id: %d \n", etudiant[j].id);
              m++;
@@ -209,7 +215,6 @@ void moyenneGenerale(){
  }
 
  void nbrChaqueDep(){
-
     char T[20][50];
     int index = 0;
     int isFound = 0;
@@ -291,6 +296,83 @@ void moyenneGenerale(){
 
  }
 
+ void nbrReussi(){
+    char T[20][50];
+    int isFound;
+    int index;
+    index = 0;
+
+    for(int i = 0 ; i < numEtediant ; i++ ){
+            isFound = 0;
+        for(int j = 0 ; j < numEtediant ; j++){
+            if( strcmp(etudiant[i].departement, T[j]) == 0 ){
+                isFound++;
+                break;
+            }
+        }
+        if(!isFound){
+            strcpy(T[index] ,etudiant[i].departement );
+            index++;
+        }
+    }
+
+    int sommeNbrRe;
+    for(int i = 0 ; i < index ; i++){
+        sommeNbrRe = 0;
+        printf("------------------------------------ \n");
+
+        for(int j = 0 ; j < numEtediant ; j++){
+            if( strcmp(T[i] , etudiant[j].departement) == 0 && etudiant[j].noteGenerale >= 10 ){
+                     sommeNbrRe++;
+                }
+        }
+            printf("le nombre d'etudiants ayant reussi dans la departement %s de : %d \n",etudiant[i].departement , sommeNbrRe);
+    }
+ }
+
+ void listeAmieChaqueDepartement(){
+   char T[20][50];
+    int index = 0;
+    int isFound = 0;
+
+    for(int i = 0 ; i < numEtediant ; i ++ ){
+        isFound = 0;
+        for(int j = 0 ; j < numEtediant ; j++){
+            if(strcmp(etudiant[i].departement, T[j] ) == 0 ){
+                isFound = 1;
+                break;
+            }
+        }
+        if(!isFound){
+            strcpy(T[index], etudiant[i].departement);
+            index++;
+        }
+    }
+
+    for(int i = 0 ; i < index ; i++){
+        isFound = 0;
+        printf("------------------------------ \n");
+        printf("liste de departement de : %s \n", T[i]);
+        printf("------------------------------ \n");
+
+        printf("eleve N : %d \n", i+1 );
+
+        for(int j = 0 ; j < numEtediant ; j++){
+            if(strcmp(T[i], etudiant[j].departement) == 0){
+                printf("le nom : %s \n", etudiant[j].nom);
+                printf("le prenom  : %s \n", etudiant[j].prenom);
+                printf("la date de naissance : %s \n", etudiant[j].dateDeNaissance);
+                printf("la note generale : %.2f \n", etudiant[j].noteGenerale);
+                printf("------------------------------ \n");
+            }
+
+        }
+
+
+    }
+
+ }
+
 int main() {
       int choix;
       do{
@@ -314,6 +396,7 @@ int main() {
     case 5 :
         moyenneGenerale();
         break;
+
         int statisticChoix;
     case 6 :
         do{
@@ -342,15 +425,22 @@ int main() {
             case 4 :
                 troisBestNotes();
                 break;
-            }
+            case 5 :
+                nbrReussi();
+                break;            }
         }while(statisticChoix < 1);
         break;
+
     case 7 :
         rechercherParLeNom();
         break;
+
+    case 8 :
+        listeAmieChaqueDepartement();
+        break;
     }
 
-}while(choix < 7);
+}while(choix < 9);
 
 return 0;
 }
